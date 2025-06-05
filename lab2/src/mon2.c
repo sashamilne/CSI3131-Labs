c14dfef5bd745e6e54fbf7e19152bc47219879fb
Sasha Milne <147281806+sashamilne@users.noreply.github.com>
2025-06-05 01:43:54 +0000

    updates

--- lab2/src/mon2.c
+++ lab2/src/mon2.c
@@ -15,7 +15,10 @@ Description: This program creates a process to run the program identified
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <signal.h>
+#include <unistd.h>
+#include <limits.h>
 
 
 /* It's all done in main */
@@ -30,9 +33,35 @@ int main(int argc, char **argv)
         program = argv[1];  /* This is the program to run and monitor */
 
 
+    char path[PATH_MAX];
+    ssize_t len = readlink("/proc/self/exe", path, sizeof(path) - 1);
+    if(len == -1)
+    {
+        perror("readlink");
+        exit(EXIT_FAILURE);
+    }
+    path[len] = '\0';
+
+    
+    char* last_slash = strrchr(path, '/');
+    if(last_slash != NULL)
+    {
+        *(last_slash+1) = '\0';
+    }
+
+
+    char program_path[PATH_MAX];
+    char procmon_path[PATH_MAX];
+    char filter_path[PATH_MAX];
+
+    snprintf(program_path, sizeof(program_path), "%s%s", path, program);
+    snprintf(procmon_path, sizeof(procmon_path), "%s%s", path, "procmon");
+    snprintf(filter_path, sizeof(filter_path), "%s%s", path, "filter");
+
+
     /* First Step: Create the first process to run the program from the command line */
 
-    pid_t program_pid = fork()
+    pid_t program_pid = fork();
 
     // If process fails to fork
     if (program_pid < 0)
@@ -43,7 +72,7 @@ int main(int argc, char **argv)
     // Child Branch
     if (program_pid == 0)
     {
-        execlp(program, program, NULL);
+        execlp(program_path, program, NULL);
         perror("Failed to execute process - program.");
         return 1;
     }
@@ -70,8 +99,14 @@ int main(int argc, char **argv)
     }
     if(filter_pid == 0)
     {
-        execlp("filter", "filter", NULL);
+        // Child: filter
+        close(pipefd[1]);                 // Close unused write end
+        dup2(pipefd[0], STDIN_FILENO);    // Redirect stdin to read from pipe
+        close(pipefd[0]);                 // Close original read end
+        
+        execlp(filter_path, "filter", NULL);
         perror("Failed to execute filter program");
+        return 1;
     }
 
     /* Fourth step: Lets create the procmon process - don't forget to connect to the pipe */
@@ -81,14 +116,22 @@ int main(int argc, char **argv)
 
     if(procmon_pid < 0)
     {
-        perror("Failed to fork process - filter");
+        perror("Failed to fork process - procmon");
         return 1;
     }
 
+    char pid_arg[20];
+    sprintf(pid_arg, "%d", program_pid);
+
     if(procmon_pid == 0)
     {
-        execlp("procmon", "procmon", NULL);
-        perror("Failed to execute filter program");
+         // Child: procmon
+        close(pipefd[0]);                 // Close unused read end
+        dup2(pipefd[1], STDOUT_FILENO);   // Redirect stdout to write to pipe
+        close(pipefd[1]);                 // Close original write end
+        
+        execlp(procmon_path, "procmon", pid_arg, NULL);
+        perror("Failed to execute process - procmon");
     }
 
 
@@ -100,10 +143,19 @@ int main(int argc, char **argv)
        2. Sleep for 2 seconds 
        3. Kill the procmon and filter processes
     */
-    kill()
+    kill(program_pid, SIGKILL);
+    waitpid(program_pid, NULL, 0);
+
+    sleep(2);
+
+    kill(procmon_pid, SIGKILL);
+    waitpid(procmon_pid, NULL, 0);
+    
+    kill(filter_pid, SIGKILL);
+    waitpid(filter_pid, NULL, 0);
 
 
-    return(0);  /* All done */
+    return 0;  /* All done */
 }
 
 
